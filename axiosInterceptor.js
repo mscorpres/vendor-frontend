9237f1a8f7cd34b9190fd6546d3c5d4a56d3d18a
devesh801015 <132462423+devesh801015@users.noreply.github.com>
2023-12-11 11:13:06 +0530

    added axios interceptor

---
+++ axiosInterceptor.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const link = "https://dev.mscorpres.net/";
+const socketLink = "http://localhost:3005";
+
+// comment the above two links and decomment below two links for production
+
+// const link = "https://api.mscorpres.net:3001/";
+// let socketLink = "https://socket.mscorpres.net:3005";
+export { socketLink, link };
+axios.defaults.baseURL = link;
+export default axios.interceptors.request.use((config) => {
+  const user = JSON.parse(localStorage.getItem("loggedInUserVendor"));
+  if (user) {
+    config.headers["x-csrf-token"] = user?.token;
+    config.headers["Company-Branch"] = "BRMSC012";
+  }
+  return config;
+});
+//
