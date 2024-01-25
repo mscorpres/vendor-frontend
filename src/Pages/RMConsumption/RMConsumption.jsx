f89e8e63f598dcbb341ca2236901e5ea2436cbe4
kanishka-mscorpes <139351665+kanishka-mscorpes@users.noreply.github.com>
2024-01-25 17:04:48 +0530

    Revert "Update RMConsumption.jsx"
    This reverts commit 21b5afa09b8dac1871929fb0177269bd5eb0b7e0.

--- src/Pages/RMConsumption/RMConsumption.jsx
+++ src/Pages/RMConsumption/RMConsumption.jsx
@@ -32,8 +32,6 @@ function RMConsumption() {
   const [asyncOptions, setAsyncOptions] = useState([]);
   const [selectLoading, setSelectLoading] = useState(false);
   const [challans, setChallans] = useState([]);
-
-  const [availQty, setAvailQty] = useState("");
   const [headerOptions, setHeaderOptions] = useState({
     jobwork: "",
     challan: "",
@@ -42,11 +40,10 @@ function RMConsumption() {
     {
       id: v4(),
       component: "",
-      location: "",
+      location: locationOptions[0]?.value,
       qty: 0,
       uom: "--",
       remark: "",
-      availableQty: "",
     },
   ]);
   const [showSubmitConfirm, setShowSubmitConfirm] = useState(false);
@@ -207,72 +204,9 @@ function RMConsumption() {
     },
     fileList,
   };
-  const getDetails = async (c, l) => {
-    // console.log("c", c.value);
-    // console.log("l", l);
-    // setPageLoading(true);
-    const { data } = await axios.post("/jwvendor/getStock", {
-      component: c.value,
-      pick_location: l,
-    });
-    rows[0].availableQty = "";
-    if (data.code === 200) {
-      // console.log("data", data);
-      rows[0].availableQty = data.data.calculated_qty;
-      // availQty = data.data.location_qty;
-      setAvailQty(data.data.calculated_qty);
-      // console.log("data.data.location_qty- >>", data.data.location_qty);
-    }
-  };
-  useEffect(() => {
-    // console.log("availQty", availQty);
-    if (availQty) {
-      // console.log("availQty inside avail Qty", availQty);
-      setAvailQty(availQty);
-      // rows[0].availableQty = availQty;
-    }
-  }, [availQty]);
 
-  useEffect(() => {
-    // console.log("rows?.pick_location", rows);
-    if (rows[0]?.component && rows[0]?.location) {
-      console.log("rows[0]?.pick_location", rows[0]?.pick_location);
-      console.log("rows[0]?.component", rows[0]?.component);
-      getDetails(rows[0].component, rows[0].location);
-    }
-  }, [rows[0]?.component, rows[0]?.location]);
-
-  const validateFields = async () => {
-    const values = await form.validateFields();
-    console.log("valuesss", values);
-    submitData(values);
-  };
-  const submitData = async (values) => {
-    console.log("rows", rows);
-
-    let payload = {
-      challan_no: values.challan_no,
-      challan_date: values.challan_date,
-      jobwork_attach: values.file,
-      component: rows.map((r) => r.component.value),
-      qty: rows.map((r) => r.qty),
-      pick_location: rows.map((r) => r.location),
-      remark: rows.map((r) => r.remark),
-    };
-    console.log("payload", payload);
-    const response = await axios.post("/jwvendor/rmConsp", payload);
-    const { data } = response;
-    console.log("data", data);
-    if (data.code === 200) {
-      toast.success(data.message);
-    } else {
-      toast.error(data.message.msg);
-    }
-  };
   const submitHandler = async () => {
-    if (showSubmitConfirm && showSubmitConfirm.fromdata) {
-    }
-    {
+    if (showSubmitConfirm) {
       let fileData;
       setSubmitLoading(true);
       const response = await axios.post(
@@ -289,9 +223,9 @@ function RMConsumption() {
       showSubmitConfirm.finalObj.jobwork_attach = uploadedFile.data;
       if (fileData.code != 200) {
         console.log("fileDatafileData", fileData);
-        // return toast.error(
-        //   "Some error occured while uploading invoices, Please try again"
-        // );
+        return toast.error(
+          "Some error occured while uploading invoices, Please try again"
+        );
       } else {
         const { data } = await axios.post(
           "/jwvendor/rmConsp",
@@ -334,7 +268,7 @@ function RMConsumption() {
       {
         id: v4(),
         component: "",
-        location: "",
+        location: locationOptions[0]?.value,
         qty: 0,
         uom: "--",
         remark: "",
@@ -394,25 +328,11 @@ function RMConsumption() {
       renderCell: ({ row }) => (
         <MySelect
           // labelInValue
+          value={locationOptions[0]?.text}
           // value={locationOptions[0]?.text}
-          // value={locationOptions[0]?.text}
-          options={locationOptions}
+          // options={locationOptions}
           onChange={(value) => {
             inputHandler("location", value, row.id);
-          }}
-        />
-      ),
-    },
-    {
-      headerName: "Available Qty",
-      width: 150,
-      renderCell: ({ row }) => (
-        <Input
-          value={row.availableQty}
-          options={locationOptions}
-          suffix={row.uom}
-          onChange={(e) => {
-            inputHandler("availableQty", e.target.value, row.id);
           }}
         />
       ),
@@ -450,6 +370,7 @@ function RMConsumption() {
 
   return (
     <div style={{ height: "90%" }}>
+      
       <SearchHeader title="Create RM Consumption" />
       {/* submit confirm modal */}
       <Modal
@@ -532,11 +453,7 @@ function RMConsumption() {
                 </Col>
                 <Col>
                   <Form.Item>
-                    <Button
-                      type="primary"
-                      htmlType="submit"
-                      onClick={() => validateFields()}
-                    >
+                    <Button type="primary" htmlType="submit">
                       Save
                     </Button>
                   </Form.Item>
